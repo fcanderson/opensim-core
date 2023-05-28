e566f8dd78ee28af448f956dc255690420fbf7ac
Clay Anderson <fcanderson@users.noreply.github.com>
2023-05-27 23:29:18 -0500

    StatesDocument: added/edited documentation

--- OpenSim/Simulation/StatesDocument.h
+++ OpenSim/Simulation/StatesDocument.h
@@ -10,7 +10,7 @@
  * through the Warrior Web program.                                           *
  *                                                                            *
  * Copyright (c) 2023 Stanford University and the Authors                     *
- * Author(s): F. C. Anderson                                                  *
+ * Author(s): Frank C. Anderson                                                  *
  *                                                                            *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may    *
  * not use this file except in compliance with the License. You may obtain a  *
@@ -30,32 +30,132 @@
 namespace OpenSim {
 
 //=============================================================================
-// StatesDocument
 //=============================================================================
-/** Class StatesDocument provides a means of serializing and deserializing a
-complete time sequence of states.
-Basic XML is used as the serialization format (@See SimTK::Xml).
+/** Class StatesDocument provides a comprehensive means of writing
+(serializing) and reading (deserializing) a time history of OpenSim::Model
+states to and from file. This capability is key, for example, when analyzing
+model behavior, visualizing simulation results, or solving optimization
+problems.
 
-Upon construction, a StatesDocument object can be relied upon to contain
-a complete XML document.
+The states of an OpenSim::Model consist of all the independent variables that
+change (or can change) during a simulation. At each time step in a simulation,
+they are captured in a SimTK::State object. Roughly speaking, state variables
+come in three categories: 1) Continuous Variables, 2) Discrete Variables, and
+3) Modeling Options.
+
+Continuous Variables, referred to in OpenSim as simply "State Variables," are
+governed by differential equations. They are numerically integrated during a
+simulation. Examples include joint angles and speeds.
+
+Discrete Variable are not governed by differential equations and so can change
+discontinuously during a simulation. Examples include inputs to a simulation
+like muscle excitations. Examples can also include outputs to a simulation
+like points of contact between colliding bodies. Output discrete variables are
+implemented as Auto-Update Discrete Variables. They are updated at each
+time step during numerical integration but based on closed-form
+algebraic expressions rather than based on values of their derivatives.
+
+Modeling Options are flags, usually of type int, that are used to choose
+between viable ways to model the System or apply a constraint. Examples include
+a flag that specifies whether Euler angles or quaternions are used to represent
+rotation or a flag that specifies whether a particular joint angle is locked.
+When a Modeling Options is changed, low-level aspects of the System must be
+reconstituted or, to use Simbody terminology, re-realized.
+
+Prior to the introduction of this class, only Continuous Variables (i.e.,
+OpenSim::StateVariable%s) were routinely and systematically serialized, most
+commonly via the OpenSim::Manager as an OpenSim::Storage file or via class
+OpenSim::StatesTrajectory as an OpenSim::TimeSeriesTable. Discrete Variables
+and Modeling Options, if serialized, had to be stored in separate files or
+handled as OpenSim::Property objects. In addition, all Discrete Variables
+in OpenSim were assumed to be type double, which is not a requirement of the
+underlying SimTK::System or SimTK::State.
+
+With this class, all state variables {i.e., Continuous States
+(aka OpenSim::StateVariable%s), Discrete Variables, and Modeling Options} can
+be serialized into the same file, which by convention has the '.ostates' file
+name exention. In addition, Discrete Variables can be a variety of types
+(e.g., bool, int, double, Vec3, etc.). Continuous States are still assumed to
+be type double, and Modeling Options are still assumed to be type int, but the
+file format has the flexibility to relax these assumptions if needed.
+
+---------------
+XML File Format
+---------------
+Basic XML is used as the organizing framework for .ostates files. See
+SimTK::Xml. Files are viewable and editable with most text editors and
+browsers. Attention to detail is critical when editing .ostates files,
+however. Successful deserialization relies on the .ostates file matching the
+intended OpenSim::Model. In particular, the model name recorded in the
+.ostates file must be the same as the OpenSim::Model name, and all variable
+paths must be resolved in the Component heirarchy. Additionally, the number of
+values for each variable in the .ostates file must be equal to the value of
+the 'nTime' attribute of the top-level 'ostates' element. Finally, the type
+of a variable must be supported type.
+
+
+A sample of portions of a .ostates file follows.
+
+
+
+
+
+
+
+
+
+------------
+Design Notes
+------------
+
+"Trajectory" ...
+
+
+Specific to a Model ...
+
+
+Construction ...
+
+
+
+
+
+In this class and in supporting classes, such a
+sequence is referred to as a 'trajectory of states' (or 'states trajectory').
+
+(OpenSim::Model is built on top of the SimTK::System)
+
+
+The SimTK::State contains all the independent variables of a SimTK::System that
+change (or can change) during a simulation. A SimTK::System
+together with its SimTK::State
+(i.e., an element of the SimTK::State) cannot be computed from other
+state variables. Every simulation must nitial values for all state variables must
+be provided as input. During a simulation, a new SimTK::State is generated
+at every time step.
+
+
+In terms of implementation
+at the lowest level, we use a SimTK::Array_<SimTK::State> as the common
+currency for passing states trajectories around OpenSim.
+
+
+
+
+Upon construction, a StatesDocument object always contains a complete XML document.
 
 An OpenSim::StatesTrajectory is a time-ordered SimTK::Vector of SimTK::State%s
 (i.e., Vector<State>), along with a number of helpful methods for accessing
 those State%s and ensuring compatibility with a particular OpenSim::Model.
 
-A SimTK::State object contains all the independent variables associated with
-a SimTK::System that change (or can change) during the course of a simulation.
-By definition, a state variable cannot be computed from other state variables.
-At the very least, initial values of all state variables must be provided as
-input to conduct a simulation. (If a quantity can be computed from the states,
-that quantity is treated as a data cache variable.)
 
---------------------------
-%State Categories
---------------------------
-The SimTK::State is comprised of variables that fall into two categories:
 
-    1) Continuous
+-------------------------------
+The SimTK::State in More Detail
+-------------------------------
+The SimTK::State is comprised of state variables that fall into two categories:
+
+    1) Continuous State Variables
     Continous states are governed by differential equations and
     rely upon numerical integration to obtain their time histories. Examples
     include generalized coordinates and speeds, among many other possibilities.
@@ -66,12 +166,12 @@ The SimTK::State is comprised of variables that fall into two categories:
     (e.g., see Component::setStateVariableValue()). They are uniformly assumed
     to be type double.
 
-    2) Discrete
+    2) Discrete State Variables
     Discrete states are not governed by differential equations
     and so can change discontinuously during a simulation. They come in three
     subcategories:
 
-        A) Modeling
+        A) Modeling Discrete State Variables
         Modeling discrete states are flags, usually of type bool
         or int, that are used to choose between viable ways to model the
         System. An example is a flag that specifies whether Euler angles or
@@ -86,7 +186,7 @@ The SimTK::State is comprised of variables that fall into two categories:
         (e.g., see Component::setModelingOption()) and are uniformly assumed
         to be type int.
 
-        B) Regular
+        B) Regular Discrete State Variables
         Regular discrete states are variables that are likely to
         change during the course of a simulation. These changes, however, do
         not require low-level re-realization of the System. Typically, regular
@@ -99,7 +199,7 @@ The SimTK::State is comprised of variables that fall into two categories:
         They can be any type that can be represented by a SimTK::Value<T>
         (e.g., bool, int, SimTK::Vec3, etc.).
 
-        C) Auto-Update
+        C) Auto-Update Discrete State Variables
         Like regular discrete states, auto-update discrete
         states are expected to change during the course of a simulation and
         these changes do not require low-level re-realiztion of the System.
@@ -170,11 +270,6 @@ public:
     StatesDocument(const OpenSim::Model& model,
         const SimTK::Array_<SimTK::State>& trajectory,
         int precision = SimTK::LosslessNumDigitsReal);
-
-    //-------------------------------------------------------------------------
-    // Accessors
-    //-------------------------------------------------------------------------
-
 
     //-------------------------------------------------------------------------
     // Serialization and Deserialization
