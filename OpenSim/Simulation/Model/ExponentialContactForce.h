945b8ccd4c58dc70bae79e140b1cfde7f1b710d5
nickbianco <nbianco@stanford.edu>
2025-06-12 14:14:34 -0500

    Update contributor name in ExponentialContactForce

--- OpenSim/Simulation/Model/ExponentialContactForce.h
+++ OpenSim/Simulation/Model/ExponentialContactForce.h
@@ -11,7 +11,7 @@
  *                                                                            *
  * Copyright (c) 2024-2025 Stanford University and the Authors                *
  * Author(s): F. C. Anderson                                                  *
- * Contributor(s): Nick Bianco                                                *
+ * Contributor(s): Nicholas Bianco                                            *
  *                                                                            *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may    *
  * not use this file except in compliance with the License. You may obtain a  *
